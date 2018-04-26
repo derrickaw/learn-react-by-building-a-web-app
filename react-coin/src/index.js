675be5ad13ea9285f46533126d3af965a6865e8c
Derrick Williams <myutat@gmail.com>
2018-04-25 21:11:59 -0700

    made minor component

---
+++ react-coin/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Component
+const App = () => {
+    return <h1>React Coin</h1>;
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
+
+
+// function App() {
+//     return <h1>React Coin</h1>;
+// }
